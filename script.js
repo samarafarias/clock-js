58eafd2d5f57ad152e2e293f64a0c817ed696188
Sam <samara000@gmail.com>
2022-02-06 22:53:45 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function updatedTime(){
+    
+    let time = document.querySelector('.time');
+
+    var realTime = new Date();
+
+    var hours = correctedTime( realTime.getHours()) + ':' + correctedTime(realTime.getMinutes()) + ':' + correctedTime( realTime.getSeconds());
+
+    time.textContent = hours;
+}
+
+function correctedTime(num){
+
+    if(num < 10){
+        num = '0' + num;
+    }
+
+    return num;
+}
+
+updatedTime();
+setInterval(updatedTime, 1000);
+

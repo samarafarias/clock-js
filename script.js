38cf9e218982c041e31cc0df5f8d8cd9e6d7611b
Sam <samara000@gmail.com>
2023-07-23 23:25:04 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function updatedTime(){
-    
     let time = document.querySelector('.time');
 
     var realTime = new Date();
-
-    var hours = correctedTime( realTime.getHours()) + ':' + correctedTime(realTime.getMinutes()) + ':' + correctedTime( realTime.getSeconds());
-
+    var hours = correctedTime(realTime.getHours()) + ':' + correctedTime(realTime.getMinutes());
     time.textContent = hours;
 }
 
 function correctedTime(num){
-
     if(num < 10){
         num = '0' + num;
     }
-
     return num;
 }
 
